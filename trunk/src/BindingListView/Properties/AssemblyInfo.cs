a0743255d763990d6213b4ee225ee66596078ae7
andrewdavey <andrewdavey@8cdcb568-0019-0410-8a2f-ea5a94585261>
2006-09-12 17:32:48 +0000

    Bump to version 1.2

--- trunk/src/BindingListView/Properties/AssemblyInfo.cs
+++ trunk/src/BindingListView/Properties/AssemblyInfo.cs
@@ -30,7 +30,7 @@ using System.Runtime.InteropServices;
 //      Build Number
 //      Revision
 //
-[assembly: AssemblyVersion("1.1.*")]
-[assembly: AssemblyFileVersion("1.1.3.1")]
+[assembly: AssemblyVersion("1.2.*")]
+[assembly: AssemblyFileVersion("1.2.0.0")]
 
 [assembly: CLSCompliant(true)]
