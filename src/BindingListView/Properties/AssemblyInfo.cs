14b75dd31601376bcac0797deedfb3d6d21a6567
Christian Junk <christian.junk@amecfw.com>
2017-02-25 17:59:31 +0100

    Upgrade to Visual Studio 2010.

--- src/BindingListView/Properties/AssemblyInfo.cs
+++ src/BindingListView/Properties/AssemblyInfo.cs
@@ -31,6 +31,6 @@ using System.Runtime.InteropServices;
 //      Revision
 //
 [assembly: AssemblyVersion("1.2.*")]
-[assembly: AssemblyFileVersion("1.2.1.0")]
+[assembly: AssemblyFileVersion("1.2.2.0")]
 
 [assembly: CLSCompliant(true)]
