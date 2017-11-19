a6624c0abe95eb406316e41cc55be72d6a870dee
Christian Junk <christian.junk@amecfw.com>
2017-11-19 10:43:18 +0100

    Updated Assembly and File Version.

--- src/BindingListView/Properties/AssemblyInfo.cs
+++ src/BindingListView/Properties/AssemblyInfo.cs
@@ -30,7 +30,7 @@ using System.Runtime.InteropServices;
 //      Build Number
 //      Revision
 //
-[assembly: AssemblyVersion("1.2.*")]
-[assembly: AssemblyFileVersion("1.2.2.0")]
+[assembly: AssemblyVersion("1.3.*")]
+[assembly: AssemblyFileVersion("1.3.0.0")]
 
 [assembly: CLSCompliant(true)]
