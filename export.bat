547110617018e2ec44fd5c3507759387f077288b
Christian Junk <christian.junk@geomatics.io>
2017-12-31 18:13:50 +0100

    Removed old build files.

--- export.bat
+++
@@ -1,2 +0,0 @@
-rmdir /S /Q export
-svn export https://svn.sourceforge.net/svnroot/blw/trunk export
--- make.bat
+++
@@ -1,8 +0,0 @@
-set path=C:\Program Files\Microsoft Visual Studio 8\Common7\IDE;C:\Program Files\Microsoft Visual Studio 8\VC\BIN;C:\Program Files\Microsoft Visual Studio 8\Common7\Tools;C:\Program Files\Microsoft Visual Studio 8\SDK\v2.0\bin;C:\WINDOWS\Microsoft.NET\Framework\v2.0.50727;C:\Program Files\Microsoft Visual Studio 8\VC\VCPackages;C:\program files\texmf\miktex\bin;C:\WINDOWS\system32;C:\WINDOWS;C:\WINDOWS\System32\Wbem;C:\Program Files\Executive Software\Diskeeper\;C:\Program Files\Subversion\bin;c:\Program Files\Java\jdk1.5.0_06\bin\
-mkdir "bin"
-
-pushd .
-cd src\BindingListView
-msbuild /p:Configuration=Release;OutDir=..\..\bin\
-popd
-
